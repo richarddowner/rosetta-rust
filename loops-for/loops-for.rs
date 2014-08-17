7f91f465ae5954ea90b2ff25a2228996b4127526
Richard Downer <richarddowna@gmail.com>
2014-08-18 09:18:04 +1200

    loops-for

---
+++ loops-for/loops-for.rs
@@ -0,0 +1,15 @@
+// Show how two loops may be nested within each other, with
+// the number of iterations performed by the inner loop being
+// controlled by the outer loop.
+//
+// Print this pattern:
+// *
+// **
+// ***
+// ****
+// *****
+fn main() {
+	for i in range(1u, 5) {
+		
+	}
+}
