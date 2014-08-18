3c2f753c200bcd56e4c9835421260c5af5c4fcc7
Richard Downer <richarddowna@gmail.com>
2014-08-18 18:20:16 +1200

    happy-numbers - init

---
+++ happy-numbers/happy-numbers.rs
@@ -0,0 +1,29 @@
+// A happy number is defined by the following process.
+// Starting with any positive int, replace the number
+// by the sum of the squares of its digits, and repeat
+// until the number equals 1, or it loops endlessly in
+// a cycle which does not include 1.
+// Those numbers for which this process ends in 1 are
+// happy numbers.
+
+fn happy_nums(input: uint) -> uint {
+	let mut tenths = 0u;
+	tenths = input%10;
+	tenths
+}
+
+fn main(){
+	println!("{}", happy_nums(19));
+}
+
+#[test]
+fn first_eight_happy_nums_test() {
+	assert_eq!(happy_nums(1), 1);
+	assert_eq!(happy_nums(7), 1);
+	assert_eq!(happy_nums(10), 1);
+	assert_eq!(happy_nums(13), 1);
+	assert_eq!(happy_nums(19), 1);
+	assert_eq!(happy_nums(23), 1);
+	assert_eq!(happy_nums(28), 1);
+	assert_eq!(happy_nums(31), 1);
+}
