42eb8aae92bc1e34a8bd5487eb9a4fc842db9721
Richard Downer <richarddowna@gmail.com>
2014-11-02 21:39:46 -0500

    cannot assign to immutable vec content list

--- mergesort/src/main.rs
+++ mergesort/src/main.rs
@@ -1,4 +1,4 @@
-fn mergesort(left: uint, right: uint, list:&[int]) -> () {
+fn mergesort(left: uint, right: uint, list:&[uint]) -> () {
 	// base case
 	if(right - left <= 1) { 
 		return; 
@@ -18,18 +18,45 @@ fn mergesort(left: uint, right: uint, list:&[int]) -> () {
 	merge(list, l_start, l_end, r_start, r_end);	
 }
 
-fn merge(list:&[int], l_start: uint, l_end:uint, r_start:uint, r_end:uint) -> () {
-	// temp list sizes
-	let l_len = l_end - l_start;
-	let r_len = r_end - r_start;
+fn merge(list:&[uint], l_start: uint, l_end:uint, r_start:uint, r_end:uint) -> () {
 
 	// temp lists for comparison
-	let mut l_half: [uint, ..l_len];
-	let mut r_half: [uint, ..r_len];	
+	let mut l_half: Vec<uint> = Vec::new();
+	let mut r_half: Vec<uint> = Vec::new();
+
+	let mut i = l_start;
+	let mut l = 0_u;
+	let mut r = 0_u;
+
+	// copy values into temp lists
+	while i < l_end {
+		l_half.insert(l, list[i] as uint);
+		i = i + 1; l = l + 1;
+	}
+	i = r_start;
+	while i < r_end {
+		r_half.insert(r, list[i] as uint);
+		i = i + 1; r = r + 1;
+	}
+
+	// merge the values back into positions in the main list
+	i = l_start; r = 0_u; l = 0_u;
+	while l < l_half.len() && r < r_half.len() {
+		// if left value < r value, move left value
+		if(l_half[l] < r_half[r]) {
+			list[i] = l_half[l];
+			l = l + 1;
+		} else {
+			list[i] = r_half[r];
+			r = r + 1;
+		}		
+		i = i + 1;
+	}
+
 }
 
 //#[test]
 fn main() {
-	let mut unsorted: [int, ..10] = [1, 5, 2, 9, 8, 4, 3, 6, 7, 0];
+	let mut unsorted: [uint, ..10] = [1, 5, 2, 9, 8, 4, 3, 6, 7, 0];
 	mergesort(0, unsorted.len(), &unsorted);
 }
