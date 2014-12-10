80d6c16d2067fe5d16c9cdf83be2a76b5b60af95
Richard Downer <richarddowna@gmail.com>
2014-12-09 19:49:05 -0500

    c# mergesort port complete

--- mergesort2/src/main.rs
+++ mergesort2/src/main.rs
@@ -1,47 +1,38 @@
-fn main() {
-	let mut list: Vec<u64> = vec![1_u64, 5, 2, 7, 3, 9, 4, 6];
-	println!("unsorted list: {}", list);
-	let sortedlist = mergesort(&mut list);
-	println!("sorted list:   {}", sortedlist);
+fn sort<T: Clone + PartialOrd>(list: &[T]) -> Vec<T> {
+	if list.len() <= 1 {
+		return list.to_vec();	
+	}	
+	let left = list.slice(0u, (list.len() / 2));
+	let right = list.slice(left.len(), list.len());
+	return merge(sort(left).as_slice(), sort(right).as_slice());
 }
-
-fn mergesort<T: Clone>(list: &mut Vec<T>) -> &mut Vec<T> {
-	if list.len() <= 1 { 
-		return list;
+fn merge<T: Clone + PartialOrd>(left: &[T], right: &[T]) -> Vec<T> {
+	let mut result = Vec::with_capacity(left.len() + right.len()); 
+	let mut left_pos = 0u;
+	let mut right_pos = 0u;
+	while left_pos < left.len() && right_pos < right.len() {
+		if left[left_pos].le(&right[right_pos]) {
+			result.push(left[left_pos].clone());
+			left_pos += 1; 
+		} else {
+			result.push(right[right_pos].clone());
+			right_pos += 1; 
+		}
 	}
-
-	let (mut left, mut right) = list.partitioned(|&n| n > 0 && n < (list.len() as u64 /2));
-	return list;
+	while left_pos < left.len() {
+		result.push(left[left_pos].clone());
+		left_pos += 1;
+	}	
+	while right_pos < right.len() {
+		result.push(right[right_pos].clone());
+		right_pos += 1;
+	}
+	return result;
 }
-/*
-
-public static List<T> Sort<T>(List<T> list) where T : IComparable
-        {
-            if (list.Count <= 1) return list;
- 
-            List<T> left = list.GetRange(0, list.Count / 2);
-            List<T> right = list.GetRange(left.Count, list.Count - left.Count);
-            return Merge(Sort(left), Sort(right));
-        }
- 
-        public static List<T> Merge<T>(List<T> left, List<T> right) where T : IComparable
-        {
-            List<T> result = new List<T>();
-            while (left.Count > 0 && right.Count > 0)
-            {
-                if (left[0].CompareTo(right[0]) <= 0)
-                {
-                    result.Add(left[0]);
-                    left.RemoveAt(0);
-                }
-                else
-                {
-                    result.Add(right[0]);
-                    right.RemoveAt(0);
-                }
-            }
-            result.AddRange(left);
-            result.AddRange(right);
-            return result;
-        }
-*/
+#[test]
+fn mergesort_test() {
+	let list: [int, ..10] = [2, 3, 1, 8, 6, 5, 9, 4, 10, 7];
+	let correct_order: [int, ..10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	let sorted_list = sort(list);
+	assert_eq!(sorted_list, correct_order.to_vec()); 
+}
